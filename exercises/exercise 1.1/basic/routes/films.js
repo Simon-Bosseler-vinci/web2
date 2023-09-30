3cd74b3dd0f2eeeb57cc8e1985a0c2cc4226d7b0
Simon <simon.bosseler@student.vinci.be>
2023-09-30 16:20:45 +0200

    tutorials 1.7 => Create persistence

--- exercises/exercise 1.1/basic/routes/films.js
+++ exercises/exercise 1.1/basic/routes/films.js
@@ -47,10 +47,11 @@ router.get('/',(req,res,next) =>{
     
     const orderByDuration = req?.query['minimum-duration'] ? req.query['minimum-duration']  : undefined; // les [] permettent d'accéder au propriété d'un objet en évitant l'opérateur "-"
 
+    if(orderByDuration <=0)return res.sendStatus(400); // vérification du paramètre
+
     let orderFilm;
     if(orderByDuration){
-        orderFilm = [...FILMS].filter(film => film.duration >= orderByDuration); // on filtre dans une copie du tableau tous les films plus
-                                                                                // grand que la variable orderByDuration
+        orderFilm = [...FILMS].filter(film => film.duration >= orderByDuration); // on filtre dans une copie du tableau tous les films plus grand que la variable orderByDuration
     }
     res.json(orderFilm ?? FILMS); // on renvoie le filtre et s'il est undefined, on renvoie le tableau par défaut
 });
@@ -77,9 +78,75 @@ router.post('/',(req,res,next) => {
         link : link
     };
 
-    FILMS.push(newFilm);
+     const result = FILMS.find(f => f.title === newFilm.title) // vérification d'un titre unique lors de l'ajout
+     if(result) return res.sendStatus(409);
+
+       FILMS.push(newFilm);
 
     res.json(newFilm);
+});
+
+router.delete('/:id',(req,res,next) => {
+
+    const filmID = FILMS.findIndex(film => film.id == req.params.id);
+
+    if(filmID < 0)return res.sendStatus(404);
+    
+    const filmRemoveFromFILMS = FILMS.splice(filmID,1); // renvoie d'un tableau contenant tous éléments supprimés (le chiffre est le nbr d'éléments derrière l'index)
+    const filmRemove = filmRemoveFromFILMS[0];
+
+    res.json(filmRemove);
+});
+
+router.patch('/:id',(req,res,index)=>{
+    
+    const title = req?.body?.title !==0 ? req.body.title : undefined; // vérification des paramètres
+    const duration = req?.body?.duration !== 0 ? req.body.duration : undefined;
+    const budget = req?.body?.budget !== 0 ? req.body.budget : undefined;
+    const link = req?.body?.link !== 0 ? req.body.link : undefined;
+
+    if(!title && !duration && !budget && !link) return res.sendStatus(400); 
+
+    const indexFilm = FILMS.findIndex(f => f.id == req.params.id); // vérification de l'ID
+    if(indexFilm < 0)return res.sendStatus(404);
+
+    const updatedFilm = {...FILMS[indexFilm],...req.body}; // on écrase les anciennes valeurs dans une copie du tableau
+    FILMS[indexFilm] = updatedFilm; // on met à jour les nouvelles valeurs dans le tableau au bon index
+
+    res.json(updatedFilm);
+});
+
+
+router.put('/:id',(req,res) =>{
+    
+    const title = req?.body?.title !==0 ? req.body.title : undefined; // vérification des paramètres
+    const duration = req?.body?.duration !== 0 ? req.body.duration : undefined;
+    const budget = req?.body?.budget !== 0 ? req.body.budget : undefined;
+    const link = req?.body?.link !== 0 ? req.body.link : undefined;
+
+    if(!title || !duration || !budget || !link) return res.sendStatus(400); // s'il manque un seul paramètre
+
+     
+         
+    const findId = FILMS.findIndex(f =>f.id == req.params.id); // si l'on trouve l'ID, on met à jour, sinon on ajoute
+    if(findId >= 0){ // findIndex ressort -1 lorsqu'elle ne trouve rien 
+        const update = {...FILMS[findId],...req.body};
+        FILMS[findId] = update;
+        return res.json(update);
+    }
+    
+        const newFilm = {
+            id : Number(req.params.id),
+            title : title,
+            duration : duration,
+            budget : budget,
+            link : link
+        }
+
+            FILMS.push(newFilm);
+
+     return res.json(newFilm);
+        
 });
 
 module.exports = router;
