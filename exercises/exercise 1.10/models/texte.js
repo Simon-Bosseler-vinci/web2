4d9518a9d3273f47750e28d92798f34982bb89ab
Simon <simon.bosseler@student.vinci.be>
2023-10-07 15:00:21 +0200

    update to 1.10

--- exercises/exercise 1.10/models/texte.js
+++ exercises/exercise 1.10/models/texte.js
@@ -1,110 +1,111 @@
-
 const path = require('node:path');
-const { v4 } = require('uuid');
+// const { v4 } = require('uuid'); // permet de générer des id de grande taille
 const { serialize, parse } = require('../utils/json');
-
 
 const jsonDbPath = path.join(__dirname, '/../data/texts.json');
 
 const TEXTES = [
-    {
-        id : "caaf748a-0fc6-4fea-b7a6-750e33426e12",
-        content : "Apprendre à taper sur un clavier à l'aveugle",
-        level : "easy"
-    },
-    {
-        id : "7456fd08-6405-4831-96b2-c80722ba9924",
-        content : "Apprendre à taper en un temps chronometrer",
-        level : "medium"
-    },
-    {
-        id : "0fb14334-e23d-4300-a3f1-03e6f8634ad1",
-        content : "Taper un texte imposé le plus vite",
-        level : "hard"
-    }
+  {
+    id: '1',
+    content: "Apprendre à taper sur un clavier à l'aveugle",
+    level: 'easy',
+  },
+  {
+    id: '2',
+    content: 'Apprendre à taper en un temps chronometrer',
+    level: 'medium',
+  },
+  {
+    id: '3',
+    content: 'Taper un texte imposé le plus vite',
+    level: 'hard',
+  },
 ];
 
-function readAllTexte(level){
-    const textes = parse(jsonDbPath,TEXTES);
+function readAllTexte(level) {
+  const textes = parse(jsonDbPath, TEXTES); // récupération du fichier ou du tableau par défault
 
-    const tabOfLevel = ["easy","medium","hard"];
-    let orderTexte;
-    if(!level){
-        orderTexte = textes; // cas d'aucune entrée dans l'URL
-    }else if(tabOfLevel.includes(level)){
-            orderTexte = textes.filter( t => t.level === level); // cas du level trouvé dans le tableau
-        }else{
-            orderTexte = textes; // si l'entrée n'est pas dans le tableau
-        }
-    return orderTexte;
-};
+  const tabOfLevel = ['easy', 'medium', 'hard'];
+  let orderTexte;
+  if (!level) { // cas d'aucune entrée dans l'URL (lecture simple de tous les textes)
+    orderTexte = textes; 
+  } else if (tabOfLevel.includes(level)) {
+    orderTexte = textes.filter((t) => t.level === level); // cas du level trouvé dans le tableau
+  } else {
+    return undefined;
+  }
+  return orderTexte;
+}
 
-function readOneTexte(id){
-    const textes = parse(jsonDbPath,TEXTES);
+function readOneTexte(id) {
+  const textes = parse(jsonDbPath, TEXTES);
 
-    const texteFound = textes.findIndex(texte => texte.id === id);
-    if(texteFound < 0 )return undefined; // cas de l'ID non trouvé
+  const texteFound = textes.findIndex((texte) => texte.id === id);
+  if (texteFound < 0) return undefined; // cas de l'ID non trouvé
 
-    return textes[texteFound];
-};
+  return textes[texteFound];
+}
 
-function createOneTexte(content, level){
+function createOneTexte(content, level) {
+  const textes = parse(jsonDbPath, TEXTES);
+  
+const nextID = textes.length +1 ;
+  const newTexte = {
+    id: String(nextID), 
+    content,
+    level,
+  };
 
-    const textes = parse(jsonDbPath,TEXTES);
+  const sameID = textes.find(texte => texte.id === newTexte.id);
+  if(sameID) return undefined; // cas du contenu similaire
 
+  textes.push(newTexte);
+
+  serialize(jsonDbPath, textes);
+
+  return newTexte;
+}
+
+function deleteOneTexte(id) {
+  const textes = parse(jsonDbPath, TEXTES);
+
+  const deleteTexte = textes.findIndex(texte => texte.id === id);
+  if (deleteTexte < 0) return undefined;
+
+  const texteRemoveFromTEXTES = textes.splice(deleteTexte, 1); // renvoie d'un tableau contenant tous éléments supprimés (le chiffre est le nbr d'éléments derrière l'index)
+  const texteRemove = texteRemoveFromTEXTES[0];
+
+  serialize(jsonDbPath, textes);
+
+  return texteRemove;
+}
+
+function updatedOrCreateTexte(id, propertiesToUpdate) {
+  const textes = parse(jsonDbPath, TEXTES);
+
+  const findId = textes.findIndex((texte) => texte.id === id);
+
+  if (findId < 0) {
+    // cas de l'ID inconnue, on créé à partir d'ici
     const newTexte = {
-        id : v4(),
-        content,
-        level
+      id,
+      content: propertiesToUpdate.content,
+      level: propertiesToUpdate.level,
     };
-
     textes.push(newTexte);
-    
-    serialize(jsonDbPath,textes);
-
+    serialize(jsonDbPath, textes);
     return newTexte;
-};
-
-function deleteOneTexte(id){
-    const textes = parse(jsonDbPath,TEXTES);
-
-    const deleteTexte = textes.findIndex(texte => texte.id === id);
-    if(deleteTexte < 0)return undefined;
-
-    const texteRemoveFromTEXTES = textes.splice(deleteTexte,1); // renvoie d'un tableau contenant tous éléments supprimés (le chiffre est le nbr d'éléments derrière l'index)
-    const texteRemove = texteRemoveFromTEXTES[0];
-
-    serialize(jsonDbPath,textes);
-
-    return texteRemove;
-};
-
-function updatedOrCreateTexte(id,propertiesToUpdate){
-    const textes = parse(jsonDbPath,TEXTES);
-
-    const findId = textes.findIndex(texte => texte.id === id);
-
-    if(findId < 0){ // cas de l'ID inconnue, on créé à partir d'ici
-        const newTexte = {
-            id,
-            content : propertiesToUpdate.content,
-            level : propertiesToUpdate.level
-        }
-        textes.push(newTexte);
-        serialize(jsonDbPath,textes);
-        return newTexte;
-    }
-        const update = {...textes[findId],...propertiesToUpdate};
-        textes[findId] = update;
-        serialize(jsonDbPath,textes);
-        return update;
-    
-};
+  }
+  const update = { ...textes[findId], ...propertiesToUpdate };
+  textes[findId] = update;
+  serialize(jsonDbPath, textes);
+  return update;
+}
 
 module.exports = {
-    readAllTexte,
-    readOneTexte,
-    createOneTexte,
-    deleteOneTexte,
-    updatedOrCreateTexte
-}
+  readAllTexte,
+  readOneTexte,
+  createOneTexte,
+  deleteOneTexte,
+  updatedOrCreateTexte,
+};
--- exercises/exercise 1.10/routes/texte.js
+++ exercises/exercise 1.10/routes/texte.js
@@ -1,61 +1,61 @@
 const express = require('express');
 
 const {
-    readAllTexte,
-    readOneTexte,
-    createOneTexte,
-    deleteOneTexte,
-    updatedOrCreateTexte
-  } = require('../models/texte');
+  readAllTexte,
+  readOneTexte,
+  createOneTexte,
+  deleteOneTexte,
+  updatedOrCreateTexte,
+} = require('../models/texte');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  let level;
+  if (req?.query?.value) { // cas d'affichage d'une valeur de niveau entrée dans l'URL
+    level = req.query.value;
+  } else {
+    level = undefined; // cas d'affichage par défaut
+  }
 
-router.get('/',(req,res) =>{
-
-    let level;
-    if(req?.query?.value){ // cas d'affichage d'une valeur de niveau entrée dans l'URL
-        level = req.query.value;
-    }else{
-        level = undefined; // cas d'affichage par défaut
-    }
-
-    const allTexte = readAllTexte(level);
-    return res.json(allTexte);
+  const allTexte = readAllTexte(level);
+  if(!allTexte) return res.sendStatus(400); // cas d'un level non trouvé dans le tableau
+  return res.json(allTexte);
 });
 
-router.get('/:id',(req,res)=> {
-    const texteFound = readOneTexte(req.params.id);
-    if(!texteFound) return res.sendStatus(404);
-    return res.json(texteFound);
+router.get('/:id', (req, res) => {
+  const texteFound = readOneTexte(req.params.id);
+  if (!texteFound) return res.sendStatus(404);
+  return res.json(texteFound);
 });
 
-router.post('/',(req,res) => {
-    const content = req?.body?.content !== 0 ? req.body.content : undefined;
-    const level = req?.body?.level !== 0 ? req.body.level : undefined;
+router.post('/', (req, res) => {
+  const content = req?.body?.content !== 0 ? req.body.content : undefined;
+  const level = req?.body?.level !== 0 ? req.body.level : undefined;
 
-    if(!content || !level){
-        return res.sendStatus(400);
-    }
-    
-    const newTexte = createOneTexte(content,level);
-    return res.json(newTexte);
+  if (!content || !level) {
+    return res.sendStatus(400);
+  }
+
+  const newTexte = createOneTexte(content, level);
+  if(!newTexte) return res.sendStatus(409);
+  return res.json(newTexte);
 });
 
-router.delete('/:id',(req,res) =>{
-    const deleteTexte = deleteOneTexte(req.params.id);
-    if(!deleteTexte) return res.sendStatus(404);
-    return res.json(deleteTexte);
+router.delete('/:id', (req, res) => {
+  const deleteTexte = deleteOneTexte(req.params.id);
+  if (!deleteTexte) return res.sendStatus(404);
+  return res.json(deleteTexte);
 });
 
-router.put('/:id',(req,res) => {
-    const content = req?.body?.content ? req.body.content : undefined;
-    const level = req?.body?.level ? req.body.level : undefined;
+router.put('/:id', (req, res) => {
+  const content = req?.body?.content ? req.body.content : undefined;
+  const level = req?.body?.level ? req.body.level : undefined;
 
-    if(!content || !level)return res.sendStatus(400); // Un seul paramètre vide, erreur
+  if (!content || !level) return res.sendStatus(400); // Un seul paramètre vide, erreur
 
-    const newTexte = updatedOrCreateTexte(req.params.id,{content,level})
-    return res.json(newTexte);
+  const newTexte = updatedOrCreateTexte(req.params.id, { content, level });
+  return res.json(newTexte);
 });
 
 module.exports = router;
