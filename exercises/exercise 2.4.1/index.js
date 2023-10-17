b3fec1d33abc41ee588bfc07e548beadbc779ce2
Simon <simon.bosseler@student.vinci.be>
2023-10-17 21:37:00 +0200

    2.4.2 : timed events

--- exercises/exercise 2.4.1/index.js
+++ exercises/exercise 2.4.1/index.js
@@ -7,17 +7,21 @@ btn2.addEventListener("click", countClick);
 let chrono;
 const time = 5000; // 5000ms = 5sec
 let count = 0;
+let start;
 
 function startTime() {
     chrono = setTimeout( () => {
         alert(`Game over, you did not click 10 times within 5s`);
-    }, time);
+    }, time); // si on dépasse 5sec 
+    start = new Date(); // on prend la date (le temps) du démarrage 
 }
 
 function countClick(){
     count++;
     if(count == 10){
-        clearTimeout(chrono);
-        alert(`You win ! You clicked 10 times within ${time} ms`)
+        clearTimeout(chrono); // si on atteint 10 clicks, on arrête le chrono des 5sec et on affiche un pop-up 
+        const end = new Date(); // on prend la date (le temps) lorsque l'on atteint 10 clicks
+        const t = end.getTime() - start.getTime();
+        alert(`You win ! You clicked 10 times within ${t} ms`) // on affiche le temps prit pour faire les 10 clicks
     }
 }
---
+++ exercises/exercise 2.4.2/index.js
@@ -0,0 +1,24 @@
+const red = document.querySelector(".red");
+const orange = document.querySelector(".orange");
+const green = document.querySelector(".green");
+
+red.style.background="red"; // phase initial 
+
+let phase = 1;
+setInterval(() => {
+    if (phase == 1) {
+        orange.style.background = "orange";
+        red.style.background = "white";
+    } else if (phase == 2){
+        green.style.background = "green";
+        orange.style.background = "white";
+    }else if(phase == 3){
+        orange.style.background = "orange";
+        green.style.background = "white";
+    }else if(phase == 4){
+        red.style.background = "red";
+        orange.style.background = "white";
+        phase = 0; // on revient au début (attention mettre phase 0 pour qu'à l'incrémentation, on revient à 1)
+    }
+    phase++;
+}, 1000); // au bout des 2sec, on exécute toute la méthode
