e7d092deea0becd17242db65d96658a38e9b1fbf
Simon <simon.bosseler@student.vinci.be>
2023-11-17 14:33:20 +0100

    refactor : comment the HomePage

--- exercises/exercise 2.15/src/Components/Pages/HomePage.js
+++ exercises/exercise 2.15/src/Components/Pages/HomePage.js
@@ -3,16 +3,16 @@ import {clearPage} from '../../utils/render';
 
 const HomePage = () => {
   clearPage();
-  
-  fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+  // fetch est asynchrone donc le programme n'attend pas les résultats, directement après le début de fetch, on passe à l'instruction suivante
+  fetch('https://v2.jokeapi.dev/joke/Any?type=single') // renvoie d'une promesse via la requête HTTP d'un browser vers des API
   .then((response) => {
     if(!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-    return response.json();
+    return response.json(); // si le promesse est réussite(fetch a eu un flux de données avec la restful API) on la renvoie en Json, sinon erreur 
   })
-  .then((jokes) => {
-    renderJokes(jokes);
+  .then((jokes) => { // .then() permet de chainer des traitements asynchrones (le chainage se fait si l'on reçoit tout d'abord la promesse)
+    renderJokes(jokes); // donc si et seulement si on a reçu une promesse, on passera par cet appel de méthode
   })
-  .catch((err) => {
+  .catch((err) => { // permet d'exécuter une callback lorsque la tâche asynchrone associée à la promesse échoue (affichage message d'erreur dans la console)
     console.error('HomePage::error ', err);
   })
 };
